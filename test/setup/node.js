e9f0d11649fa68f6791d838be5b0a3d1367a7d02
Riyadh Al Nur <riyadhalnur@verticalaxisbd.com>
2016-04-26 14:55:58 +0600

    Add test folder

---
+++ test/setup/node.js
@@ -0,0 +1,7 @@
+global.chai = require('chai');
+global.sinon = require('sinon');
+global.chai.use(require('sinon-chai'));
+global.should = chai.should;
+
+require('babel-core/register');
+require('./setup')();
