9c11141b15e8b0f75bc401bb4173a9f81547515e
Riyadh Al Nur <riyadhalnur@verticalaxisbd.com>
2016-05-02 20:05:41 +0600

    Fix chai should not working properly globally

--- test/setup/node.js
+++ test/setup/node.js
@@ -1,7 +1,9 @@
-global.chai = require('chai');
+let chai = require('chai');
+
+global.chai = chai;
 global.sinon = require('sinon');
 global.chai.use(require('sinon-chai'));
-global.should = chai.should;
+global.should = chai.should();
 
 require('babel-core/register');
 require('./setup')();
